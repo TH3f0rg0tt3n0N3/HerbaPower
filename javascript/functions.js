ea915681c4feb135d7b2c8b9f62f3e212aec5dce
CanUCocoNot <nikkixs@live.nl>
2019-02-18 12:32:34 +0100

    menu

---
+++ javascript/functions.js
@@ -0,0 +1,14 @@
+document.getElementById('openMenu').onclick = function() {
+    document.getElementById("mySidenav").style.width = "20%";
+    document.getElementById("overlay").style.width = "80%";
+    document.getElementById("overlay").style.visibility = "visible";
+    document.getElementById("copyright").style.visibility = "visible";
+    
+}
+
+/*het menu sluiten*/
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("overlay").style.visibility = "hidden";
+    document.getElementById("copyright").style.visibility = "hidden";
+}
