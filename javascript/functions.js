7a36e4a0283d6e25970ccfc1186de85093adc74d
Gabriel <gabriel@pouw.com>
2019-03-11 11:32:46 +0100

    Added a newitem generator

--- javascript/functions.js
+++ javascript/functions.js
@@ -19,3 +19,27 @@ function closeNav() {
  * @LastEdited:         21-02-2019
  * @Param:              none
  */
+function generateNewsItems(){
+    let newsAmount = 4;
+
+    for(let i = 0; i < newsAmount; i++){
+        document.getElementById("news").innerHTML += 
+        `<div class="newsItem">
+                <div class="newsPhotoHolder">
+                <img class="newsPhoto" src="../Logos/voorbeeldGebouw.jpg">
+            </div>
+            <div class="newsTitle">
+                <h4>
+                    Nieuwe HerbaPower webshop
+                </h4>
+            </div>
+            <div class="newsTextPreview">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </p>
+            </div>
+        </div>`;
+    }
+}
+
+generateNewsItems(); 
